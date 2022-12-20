5abb2c33bf0a7b66eb58604819e18fd5fd2cf736
Juan Covarrubias <me@juan.fi>
2022-12-20 21:43:15 +0200

    contollers/stations.ts reponse is wrapped in data field of json

--- backend/src/controllers/stations.ts
+++ backend/src/controllers/stations.ts
@@ -12,7 +12,7 @@ router.get('/', (async (request: Request, response: Response) => {
   const listRequest: ListRequest = validateListRequest(request);
   const stations = await StationsService.getMany(listRequest);
 
-  response.json(stations);
+  response.json({data:  stations });
 }) as RequestHandler);
 
 router.get('/:id', (async (request: Request, response: Response) => {
@@ -25,7 +25,7 @@ router.get('/:id', (async (request: Request, response: Response) => {
     response.status(404).send('Not found');
     return;
   }
-  response.json(station);
+  response.json({ data: station });
 }) as RequestHandler);
 
 export default router;
