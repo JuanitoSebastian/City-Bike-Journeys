21a8fe207c2933638c071c555c8f6e3c8b67dc82
Juan Covarrubias <me@juan.fi>
2023-01-20 15:17:49 +0200

    BackEnd: Install and activate cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import middleware from './utils/middleware';
+import cors from 'cors';
+
 
 import tripsRouter from './controllers/trips';
 import stationsRouter from './controllers/stations';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/trip', tripsRouter);
 app.use('/api/station', stationsRouter);
