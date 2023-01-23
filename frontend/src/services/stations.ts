73b429951d97816222a293198e6fcf1e079ae053
Juan Covarrubias <me@juan.fi>
2023-01-23 09:23:40 +0200

    FrontEnd: StationsService getStations function takes query params. Store holds state of query params.

--- frontend/src/services/stations.ts
+++ frontend/src/services/stations.ts
@@ -1,4 +1,4 @@
-import type { PagingDetails, Station } from '../utils/interfaces';
+import type { PagingDetails, Station, StationsQueryParameters } from '../utils/interfaces';
 import axios from 'axios';
 import { parseApiResponsePagingDetails, parseStationArray } from '../utils/validation';
 
@@ -6,10 +6,13 @@ const baseUrl = `${process.env.API_URL}/station`;
 
 /**
  * Fetches multiple stations from the API 
+ * @param params: Stations query parameters
  * @returns A tuple [PagingDetails, Station[]]
  */
-const getStations = async (): Promise<[PagingDetails, Station[]]> => {
-  const apiResponse = await axios.get(baseUrl);
+const getStations = async (params: StationsQueryParameters): Promise<[PagingDetails, Station[]]> => {
+  const apiResponse = await axios.get(baseUrl, 
+    { params: { order: params.order.value, language: params.language.value, order_by: params.orderBy.value, limit: params.limit, offset: params.offset  } }
+  );
   const parsedApiResponse = parseApiResponsePagingDetails(apiResponse.data);
   const stations = parseStationArray(parsedApiResponse.data);
   return [parsedApiResponse.paging, stations];
--- frontend/src/stores.ts
+++ frontend/src/stores.ts
@@ -1,9 +1,29 @@
 import { Writable, writable } from 'svelte/store';
-import type { DropDownOption, Station } from './utils/interfaces';
+import type { DropDownOption, Station, StationsQueryParameters } from './utils/interfaces';
+
+export const languageOptions: DropDownOption[] = [
+  { value: 'fi', label: 'Finnish'},
+  { value: 'en', label: 'English'},
+  { value: 'sv', label: 'Swedish'}
+];
+
+export const orderByOptions: DropDownOption[] = [
+  { value: 'id', label: 'Id'},
+  { value: 'name', label: 'Name'}
+];
+
+export const orderOptions: DropDownOption[] = [
+  { value: 'ASC', label: 'Ascending'},
+  { value: 'DESC', label: 'Descending'}
+];
+
+const stationsDefaultQueryParameters: StationsQueryParameters = {
+  limit: 20,
+  offset: 0,
+  language: languageOptions[0],
+  orderBy: orderByOptions[0],
+  order: orderOptions[0]
+};
 
 export const stations: Writable<Station[]|undefined> = writable([]);
-
-export const sortingDirections: DropDownOption[] = [
-  { value: 'ASC', label: 'Ascending' },
-  { value: 'DESC', label: 'Descending' }
-];
+export const stationsQueryParameters: Writable<StationsQueryParameters> = writable(stationsDefaultQueryParameters);
