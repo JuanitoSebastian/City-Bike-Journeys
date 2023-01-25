1f4b800b4a29d84b2f0ea58d51bec7a4d2d8b083
Juan Covarrubias <me@juan.fi>
2023-01-25 10:14:28 +0200

    FrontEnd: Add shadow to StationStatsCard.svelte

--- frontend/src/components/StationView/StationStatsCard.svelte
+++ frontend/src/components/StationView/StationStatsCard.svelte
@@ -10,7 +10,7 @@
   export let label: string;
 </script>
 
-<div class="bg-white rounded-lg flex flex-col justify-center items-start gap-1 h-24 max-h-24 px-4">
+<div class="bg-white rounded-lg flex flex-col justify-center items-start gap-1 h-24 max-h-24 px-4 shadow-sm">
   <p class="text-gray-600 font-light">{label}</p>
   <p class="text-3xl font-bold">{statistic}</p>
 </div>
