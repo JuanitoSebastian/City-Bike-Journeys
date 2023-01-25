b6293b8400429ac0b204f695c0aef94ada93a59d
Juan Covarrubias <me@juan.fi>
2023-01-25 10:50:07 +0200

    FrontEnd: Modify SideBarSection.svelte to use Divider.svelte

--- frontend/src/components/SideBar/SideBarSection.svelte
+++ frontend/src/components/SideBar/SideBarSection.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
+  import Divider from '../Divider.svelte';
   export let sectionTitle: string;
 </script>
 
 <div class="flex flex-col gap-4">
-  <div class="flex flex-row gap-2 items-center">
-    <p class="uppercase text-gray-600 text-sm">{sectionTitle}</p>
-    <div class="flex-grow border-t-2 border-gray-200"></div>
-  </div>
-  <slot></slot>
+  <Divider label={sectionTitle} />
+  <slot />
 </div>
