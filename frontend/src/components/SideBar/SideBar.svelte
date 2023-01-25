71d5138e88ee3217d528212ddc2003578f84ef1b
Juan Covarrubias <me@juan.fi>
2023-01-25 11:47:20 +0200

    FrontEnd: SideBar set min width

--- frontend/src/components/SideBar/SideBar.svelte
+++ frontend/src/components/SideBar/SideBar.svelte
@@ -4,7 +4,7 @@
   import SideBarSection from './SideBarSection.svelte';
 </script>
 
-<aside class="w-80 h-auto min-h-screen bg-white">
+<aside class="w-80 min-w-fit h-auto min-h-screen bg-white">
   <div class="p-4 sticky top-0">
     <div class="flex flex-col gap-8">
       <Link to="/">
