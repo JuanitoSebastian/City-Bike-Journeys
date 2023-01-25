aa156300796dd1f54453e3d75a3b360fde07f8cb
Juan Covarrubias <me@juan.fi>
2023-01-25 11:27:43 +0200

    FrontEnd: Add arrow-left to Icon and IconType

--- frontend/src/components/Icon.svelte
+++ frontend/src/components/Icon.svelte
@@ -1,3 +1,8 @@
+<!--
+ @component
+  An SVG icon. 
+    - iconType: Icon to display
+-->
 <script lang="ts">
   import type { IconType } from '../utils/interfaces';
   export let iconType: IconType;
@@ -97,6 +102,21 @@
         stroke-linecap="round"
         stroke-linejoin="round"
         d="M8.25 4.5l7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  {:else if iconType === 'arrow-left'}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
       />
     </svg>
   {/if}
