b4fad56abe78f16194f5b8ac85b3f3397fbee5c8
Juan Covarrubias <me@juan.fi>
2023-01-26 13:11:18 +0200

    FrontEnd: Styling StationViewStatisticsFilter

--- frontend/src/components/StationView/StationViewStatisticsFilter.svelte
+++ frontend/src/components/StationView/StationViewStatisticsFilter.svelte
@@ -10,29 +10,31 @@
   export let statsFilterEndDate: string | undefined;
 </script>
 
-<form>
-  <div class="flex flex-row gap-2 items-start">
-    <div class="flex flex-col">
-      <label for="start">Start date</label>
+<div
+  class="bg-white p-4 rounded-lg w-full flex flex-row items-center justify-start gap-4"
+>
+  <form class="flex flex-row gap-4 items-start">
+    <div class="flex flex-col gap-1">
+      <label for="start" class="text-sm font-light">Start date</label>
       <input
         id="start"
         bind:value={statsFilterStartDate}
         type="datetime-local"
         name="start"
-        class="w-44"
+        class="w-auto text-sm h-12 px-4 rounded border-gray-200 border focus:border-blue-500 focus:ring-blue-500 focus:ring-1 invalid:border-red-500 invalid:text-red-500 invalid:ring-red-500"
         max={statsFilterEndDate}
       />
     </div>
-    <div class="flex flex-col">
-      <label for="end">End date</label>
+    <div class="flex flex-col gap-1">
+      <label for="start" class="text-sm font-light">End date</label>
       <input
         id="end"
         bind:value={statsFilterEndDate}
         type="datetime-local"
         name="start"
-        class="w-44"
+        class="w-auto text-sm h-12 px-4 rounded border-gray-200 border focus:border-blue-500 focus:ring-blue-500 focus:ring-1 invalid:border-red-500 invalid:text-red-500 invalid:ring-red-500"
         min={statsFilterStartDate}
       />
     </div>
-  </div>
-</form>
+  </form>
+</div>
