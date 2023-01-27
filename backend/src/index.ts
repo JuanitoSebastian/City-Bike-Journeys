82c4796f2ef8739994cce1efd9ca22b6bb0d6177
Juan Covarrubias <me@juan.fi>
2023-01-27 16:48:31 +0200

    BackEnd: Start seeding after starting server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,10 +6,10 @@ import sanitizedConfig from './utils/config';
 const server = http.createServer(app);
 
 const start = async () => {
-  await initDatabase();
   server.listen(sanitizedConfig.PORT, () => {
     console.log(`Server running on port ${sanitizedConfig.PORT}`);
   });
+  await initDatabase();
 };
 
 void start();
