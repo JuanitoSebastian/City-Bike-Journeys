6702cfe278d7033ccac78da745f4d7e6272e49e2
Juan Covarrubias <me@juan.fi>
2023-01-27 20:48:53 +0200

    BackEnd: constants.ts default dir for .csv seeding

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -4,3 +4,4 @@ export const MIN_TRIP_LENGTH_METERS = 10;
 export const MIN_TRIP_DURATION_SECONDS = 10;
 export const DEFAULT_QUERY_LIMIT = 20;
 export const DEFAULT_LANGUAGE: Language = Language.English;
+export const DEFAULT_DIRECTORY_FOR_SEEDING_DATA = './data';
