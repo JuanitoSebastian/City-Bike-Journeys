ce54fb5e0ed7c252411c08e440603610fe8bb0a3
Juan Covarrubias <me@juan.fi>
2023-01-27 21:57:21 +0200

    BackEnd: yamlParser.ts that parser .yml to SeedingYaml object and validation

---
+++ backend/src/interfaces/SeedingYaml.ts
@@ -0,0 +1,7 @@
+
+interface SeedingYaml {
+  stations: string[];
+  trips: string[];
+}
+
+export default SeedingYaml;
---
+++ backend/src/utils/yamlParser.ts
@@ -0,0 +1,21 @@
+import { readFileSync } from 'fs';
+import { parseSeedingYamlObject } from '../validation/seeding';
+import YAML from 'yaml';
+import SeedingYaml from '../interfaces/SeedingYaml';
+import SeedingError from '../errors/seedingError';
+
+/**
+ * Parses seeding.yml file to SeedingYaml object
+ * @throws {SeedingError} if .yml contains no stations or trips
+ */
+export const parseSeedingYaml = (pathToFile: string): SeedingYaml => {
+  const file = readFileSync(pathToFile, 'utf8');
+  const unsanitizedParsed: unknown = YAML.parse(file);
+  const parsed = parseSeedingYamlObject(unsanitizedParsed);
+
+  if (parsed.stations.length === 0 || parsed.trips.length === 0) {
+    throw new SeedingError('seeding.yml has to include url to at least one set of stations and trips');
+  }
+
+  return parsed;
+};
