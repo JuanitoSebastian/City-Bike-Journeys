ee07d046d2e7da276b170a2def3c95ee4cbf46cd
Juan Covarrubias <me@juan.fi>
2023-01-28 12:42:13 +0200

    FrontEnd: Create Redirect.svelte for redirecting routes

---
+++ frontend/src/components/Redirect.svelte
@@ -0,0 +1,12 @@
+<!--
+ @component
+  Redirect user to other route.
+  - route: route to navigate user to
+-->
+<script lang="ts">
+  import { useNavigate } from 'svelte-navigator';
+  const navigate = useNavigate();
+
+  export let route: string;
+  navigate(route);
+</script>
