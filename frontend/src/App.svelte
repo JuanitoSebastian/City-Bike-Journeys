d7083a49f6984af5f6203fd796403de6d31d3d2f
Juan Covarrubias <me@juan.fi>
2023-01-28 12:32:01 +0200

    FrontEnd: Create SeedingView.svelte and implement check for seeding in App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,42 +1,56 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import { Router, Route } from 'svelte-navigator';
+  import SeedingsService from './services/seedings';
   import SideBar from './components/SideBar/SideBar.svelte';
   import StationsView from './components/StationsView/StationsView.svelte';
   import StationView from './components/StationView/StationView.svelte';
   import Tailwindcss from './components/Tailwindcss.svelte';
   import TripsView from './components/TripsView/TripsView.svelte';
+  import { seeding } from './stores';
+  import SeedingView from './components/SeedingView/SeedingView.svelte';
+
+  onMount(async () => {
+    await SeedingsService.startPollingSeedingApi();
+  });
 </script>
 
 <Router>
   <Tailwindcss />
-  <div class="flex flex-row w-full">
-    <SideBar />
-    <Route path="/trips">
-      <div class="container mx-auto my-4 px-4">
-        <TripsView />
-      </div>
-    </Route>
-    <Route path="/stations/*">
+  {#if !$seeding || $seeding.latestSeeding === null}
+    <div class="container mx-auto my-4 px-4">
+      <SeedingView />
+    </div>
+  {:else if $seeding && $seeding.latestSeeding !== null}
+    <div class="flex flex-row w-full">
+      <SideBar />
+      <Route path="/trips">
+        <div class="container mx-auto my-4 px-4">
+          <TripsView />
+        </div>
+      </Route>
+      <Route path="/stations/*">
+        <Route path="/">
+          <div class="container mx-auto my-4 px-4">
+            <StationsView />
+          </div>
+        </Route>
+        <Route path=":id" let:params>
+          <div class="container mx-auto my-4 px-4">
+            <StationView stationId={params.id} />
+          </div>
+        </Route>
+      </Route>
+      <Route path="/about">
+        <div class="container mx-auto my-4 px-4">
+          <p>App by juan.fi</p>
+        </div>
+      </Route>
       <Route path="/">
         <div class="container mx-auto my-4 px-4">
-          <StationsView />
+          <p>Home</p>
         </div>
       </Route>
-      <Route path=":id" let:params>
-        <div class="container mx-auto my-4 px-4">
-          <StationView stationId={params.id} />
-        </div>
-      </Route>
-    </Route>
-    <Route path="/about">
-      <div class="container mx-auto my-4 px-4">
-        <p>App by juan.fi</p>
-      </div>
-    </Route>
-    <Route path="/">
-      <div class="container mx-auto my-4 px-4">
-        <p>Home</p>
-      </div>
-    </Route>
-  </div>
+    </div>
+  {/if}
 </Router>
---
+++ frontend/src/components/SeedingView/SeedingView.svelte
@@ -0,0 +1,12 @@
+<!--
+  @component
+   A view to display while the database is being seeded.
+ -->
+<div class="flex flex-col items-center justify-start h-screen">
+  <div class="min-w-fit w-96 h-fit bg-white rounded-lg shadow flex flex-col items-center px-4 py-8 gap-4">
+    <h1 class="text-2xl font-bold text-gray-700">City Bike Journeys</h1>
+    <div style="border-top-color:transparent; border-bottom-color:transparent;" class="w-8 h-8 border-4 border-blue-600 rounded-full animate-spin"></div>
+    <p class="text-gray-600">Database is seeding.</p>
+    <p class="text-gray-600 text-sm">Hold tight, this might take a few minutes...</p>
+  </div>
+</div>
