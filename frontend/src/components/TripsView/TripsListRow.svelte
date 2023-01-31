05b134016d2fbb252cef817e21102f24fcd71709
Juan Covarrubias <me@juan.fi>
2023-01-31 10:39:34 +0200

    FrontEnd: numberFormatter.ts method for formatting seconds to minutes

--- frontend/src/components/TripsView/TripsListRow.svelte
+++ frontend/src/components/TripsView/TripsListRow.svelte
@@ -3,13 +3,18 @@
   A single row of TripsList. 
   - trip: Trip object to display
 -->
-
 <script lang="ts">
   import type { Trip } from '../../utils/interfaces';
+  import {
+    formatMeters,
+    formatSecondsToMinutes,
+  } from '../../utils/numberFormatter';
   export let trip: Trip;
 
   $: startDate = new Date(trip.startTime);
   $: endDate = new Date(trip.endTime);
+  $: distanceFormatted = formatMeters(trip.distanceMeters);
+  $: durationFormatted = formatSecondsToMinutes(trip.durationSeconds);
   const optionsTime = { timeStyle: 'short' };
   const optionsDate = { year: 'numeric', month: 'numeric', day: 'numeric' };
 </script>
@@ -17,8 +22,12 @@
 <tr class="h-16 border-t border-gray-100">
   <td>
     <div class="flex flex-col justify-center items-start">
-      <p class="text-sm">{startDate.toLocaleTimeString('fi-FI', optionsTime)}</p>
-      <p class="text-xs">{startDate.toLocaleDateString('fi-FI', optionsDate)}</p>
+      <p class="text-sm">
+        {startDate.toLocaleTimeString('fi-FI', optionsTime)}
+      </p>
+      <p class="text-xs">
+        {startDate.toLocaleDateString('fi-FI', optionsDate)}
+      </p>
     </div>
   </td>
   <td>
@@ -29,14 +38,11 @@
   </td>
   <td>{trip.startStation}</td>
   <td>{trip.endStation}</td>
+  <td>{distanceFormatted}</td>
   <td>
     <div class="flex flex-col justify-center items-start text-sm">
-      <p class="text-sm">{trip.distanceMeters}</p>
-      <p class="text-xs">meters</p>
+      <p>{durationFormatted[0]} minutes</p>
+      <p>{durationFormatted[1]} seconds</p>
     </div>
   </td>
-  <td><div class="flex flex-col justify-center items-start text-sm">
-    <p class="text-sm">{trip.durationSeconds}</p>
-    <p class="text-xs">seconds</p>
-  </div></td>
 </tr>
